cda9f7ff2ffcccee63b3c752cf601f3e036dc614
Debela Haile Silase <129580219+debudebuye@users.noreply.github.com>
2024-02-22 23:25:46 -0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 // require('env').config();
+>>>>>>> 2dddf466c25ae135e22c8e1b4398370d9c53724b
+=======
+// require('env').config();
+>>>>>>> 2dddf466c25ae135e22c8e1b4398370d9c53724b
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> 2dddf466c25ae135e22c8e1b4398370d9c53724b
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello SMIS project');
 });
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} http://localhost:3001/`);
+=======
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} http://localhost:3000/`);
+>>>>>>> 2dddf466c25ae135e22c8e1b4398370d9c53724b
+=======
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} http://localhost:3000/`);
+>>>>>>> 2dddf466c25ae135e22c8e1b4398370d9c53724b
 });
